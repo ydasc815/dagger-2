59a3fb487667681f6e38c5a28bd9363f484f78cb
Aditya <icgadi99@gmail.com>
2021-04-07 15:39:25 +0530

    --updated: di on glide

--- dagger-kotlin/app/src/main/java/com/aditya/dagger_kotlin/AuthActivity.kt
+++ dagger-kotlin/app/src/main/java/com/aditya/dagger_kotlin/AuthActivity.kt
@@ -1,18 +1,33 @@
 package com.aditya.dagger_kotlin
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import com.aditya.dagger_kotlin.utils.log
+import androidx.databinding.DataBindingUtil
+import com.aditya.dagger_kotlin.databinding.ActivityAuthBinding
+import com.bumptech.glide.RequestManager
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
 class AuthActivity : DaggerAppCompatActivity() {
 
+    private lateinit var binding: ActivityAuthBinding
+
     @Inject
-    lateinit var injectorString: String
+    lateinit var logo: Drawable
+
+    @Inject
+    lateinit var requestManager: RequestManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
-        log(injectorString)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_auth)
+
+        setLogo()
+    }
+
+    private fun setLogo() {
+        requestManager
+            .load(logo)
+            .into(binding.image)
     }
 }
--- dagger-kotlin/app/src/main/java/com/aditya/dagger_kotlin/di/AppModule.kt
+++ dagger-kotlin/app/src/main/java/com/aditya/dagger_kotlin/di/AppModule.kt
@@ -1,5 +1,12 @@
 package com.aditya.dagger_kotlin.di
 
+import android.app.Application
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.aditya.dagger_kotlin.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
 import dagger.Module
 import dagger.Provides
 
@@ -7,6 +14,18 @@ import dagger.Provides
 class AppModule {
 
     @Provides
-    fun injectTestString(): String = "Injecting Test String via App Module"
+    fun provideRequestOptions(): RequestOptions = RequestOptions()
+        .placeholder(R.drawable.image)
+        .error(R.drawable.ic_launcher_background)
 
+    @Provides
+    fun provideGlideInstance(application: Application, requestOptions: RequestOptions): RequestManager {
+        return Glide.with(application)
+            .setDefaultRequestOptions(requestOptions)
+    }
+
+    @Provides
+    fun provideAppDrawable(application: Application): Drawable {
+        return ContextCompat.getDrawable(application, R.drawable.image)!!
+    }
 }
